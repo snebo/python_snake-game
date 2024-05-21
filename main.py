1aac0629ea60bbc9c626eea4112d5b4133a5a033
Stefano <30480337+snebo@users.noreply.github.com>
2024-05-21 15:57:53 +0100

    refactored code add check collision add growth on food consumption fixed bugs

--- main.py
+++ main.py
@@ -1,107 +1,94 @@
-"""
-snake game
-tkinter is used here to provide the gui
-"""
-import tkinter
+import tkinter as tk
 import random
 
-# initilize
+# Constants
 TILE_SIZE = 25
 ROWS = 30
-COL = 30
+COLS = 30
 bg_color = "white"
-win_width = TILE_SIZE*ROWS #750 cause of the screen i'm using
-win_height = TILE_SIZE*COL
-vel_x, vel_y = 0,0
-snake_body=[]
+win_width = TILE_SIZE * COLS
+win_height = TILE_SIZE * ROWS
+INITIAL_VELOCITY = (0, 0)
+INITIAL_SNAKE_LENGTH = 1
 
 class Tile:
-    def __init__(self, x , y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
-# create game window
-window = tkinter.Tk()
-window.title("snake game")
-window.resizable(False,False)
+class SnakeGame:
+    def __init__(self, window):
+        self.window = window
+        self.window.title("Snake Game")
+        self.window.resizable(False, False)
+        
+        self.canvas = tk.Canvas(window, background=bg_color, width=win_width, height=win_height, borderwidth=0, highlightthickness=0)
+        self.canvas.pack()
+        
+        self.set_window_position()
+        
+        self.vel_x, self.vel_y = INITIAL_VELOCITY
+        self.snake_body = [Tile(5 * TILE_SIZE, 5 * TILE_SIZE)]
+        self.food = Tile(random.randint(0, COLS - 1) * TILE_SIZE, random.randint(0, ROWS - 1) * TILE_SIZE)
+        
+        self.window.bind("<KeyPress>", self.changedir)
+        self.draw()
 
-# draw on gui
-canvas = tkinter.Canvas(window, background=bg_color, width=win_width, height=win_height, borderwidth=0, highlightthickness=0)
-canvas.pack() #applies the setting to the canva
-window.update() # updates the window information
+    def set_window_position(self):
+        win_w = self.window.winfo_width()
+        win_h = self.window.winfo_height()
+        scr_w = self.window.winfo_screenwidth()
+        scr_h = self.window.winfo_screenheight()
+        win_x = int((scr_w / 2) - (win_w / 2))
+        win_y = int((scr_h / 2) - (win_h / 2))
+        self.window.geometry(f"{win_w}x{win_h}+{win_x}+{win_y}")
 
-# set default window position
-win_w = window.winfo_width() #get the window dimensions
-win_h = window.winfo_height()
-scr_w = window.winfo_screenwidth() #get the screen dimensions
-scr_h = window.winfo_screenheight()
-
-win_x = int((scr_w/2)- (win_w/2))
-win_y = int((scr_h/2) -(win_h/2))
-# moves the midpoint of the window to the midpoint of the screen
-
-window.geometry(f"{win_w}x{win_h}+{win_x}+{win_y}") #sets the new screen pos
-
-#draw function
-def draw():
-    canvas.delete("all")
-    move()
-
-    global food
-    canvas.create_rectangle(food.x, food.y, food.x+TILE_SIZE, food.y+TILE_SIZE, fill="red")
-
-    global snake 
-    canvas.create_rectangle(snake.x, snake.y, snake.x+TILE_SIZE, snake.y+TILE_SIZE, fill="blue")
-
-    #draw the snake body
-    for tile in snake_body:
-        canvas.create_rectangle(tile.x, tile.y, tile.x+TILE_SIZE, tile.y+TILE_SIZE, fill = "blue")
-
-    window.after(100, draw) #after 100ms, draw again
-
-#movement
-def changedir(e):
-    global vel_x, vel_y
-    # print(e) #debug key pressed
-    if e.keysym == "Up":
-       vel_x=0
-       vel_y=-1
-    elif e.keysym == "Down":
-       vel_x=0
-       vel_y=1
-    elif e.keysym == "Left":
-       vel_x=-1
-       vel_y=0
-    elif e.keysym == "Right":
-       vel_x=1
-       vel_y=0
-
-def move():
-    global snake, vel_y,vel_x,food, snake_body
-    snake.x+= vel_x*TILE_SIZE
-    snake.y += vel_y*TILE_SIZE
-
-    if(snake.x == food.x and snake.y== food.y):
-        snake_body.append(Tile(food.x, food.y))
-
-        #remove and reset food
-        food.x = random.randint(0, ROWS-1)*TILE_SIZE
-        food.y = random.randint(0, COL-1)*TILE_SIZE
-
-    for i in range(len(snake_body)-1, -1, -1):
-        tile = snake_body[i]
-        if(i==0):
-            tile.x = snake.x
-            tile.y = snake.y
+    def draw(self):
+        self.canvas.delete("all")
+        self.move()
+        self.canvas.create_rectangle(self.food.x, self.food.y, self.food.x + TILE_SIZE, self.food.y + TILE_SIZE, fill="red")
+        
+        for tile in self.snake_body:
+            self.canvas.create_rectangle(tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill="blue")
+        
+        if self.check_collision():
+            self.canvas.create_text(win_width // 2, win_height // 2, text="Game Over", font=('Arial', 30), fill='red')
         else:
-            prev_tile = snake_body[i-1]
-            tile.x = prev_tile.x
-            tile.y = prev_tile.y
+            self.window.after(100, self.draw)
 
-# create snake and food
-snake = Tile(5*TILE_SIZE, 5*TILE_SIZE)
-food = Tile(12*TILE_SIZE, 6*TILE_SIZE)
+    def changedir(self, e):
+        if e.keysym == "Up" and self.vel_y == 0:
+            self.vel_x, self.vel_y = 0, -1
+        elif e.keysym == "Down" and self.vel_y == 0:
+            self.vel_x, self.vel_y = 0, 1
+        elif e.keysym == "Left" and self.vel_x == 0:
+            self.vel_x, self.vel_y = -1, 0
+        elif e.keysym == "Right" and self.vel_x == 0:
+            self.vel_x, self.vel_y = 1, 0
 
-draw()
-window.bind("<KeyRelease>", changedir)
-window.mainloop()
+    def move(self):
+        new_head = Tile(self.snake_body[0].x + self.vel_x * TILE_SIZE, self.snake_body[0].y + self.vel_y * TILE_SIZE)
+        
+        if new_head.x == self.food.x and new_head.y == self.food.y:
+            self.food = Tile(random.randint(0, COLS - 1) * TILE_SIZE, random.randint(0, ROWS - 1) * TILE_SIZE)
+        else:
+            self.snake_body.pop()
+
+        self.snake_body.insert(0, new_head)
+
+    def check_collision(self):
+        head = self.snake_body[0]
+        
+        if head.x < 0 or head.x >= win_width or head.y < 0 or head.y >= win_height:
+            return True
+        
+        for tile in self.snake_body[1:]:
+            if tile.x == head.x and tile.y == head.y:
+                return True
+        
+        return False
+
+if __name__ == "__main__":
+    window = tk.Tk()
+    game = SnakeGame(window)
+    window.mainloop()
